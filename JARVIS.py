0081edbc77bf44c5d72d5dfd772812f5df84fb24
Vecro <vlad4ic1986@gmail.com>
2023-06-19 19:37:57 +0300

    add def talk

--- JARVIS.py
+++ JARVIS.py
@@ -1,1 +1,21 @@
 import speech_recognition as sr
+import os
+import sys
+import pyttsx3
+# Якщо немає звуку
+
+engine = pyttsx3.init()
+engine.say("Say")
+engine.runAndWait()
+
+
+def talk(words):
+    print(words)
+    engine = pyttsx3.init()
+    engine.say(words)
+    engine.runAndWait()
+    #os.system(" say " + words)
+
+
+talk("Hi, can I help you?")
+
